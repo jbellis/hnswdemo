8ddcda71754bffb05362b3db46342b6d204fb4bd
Jonathan Ellis <jbellis@datastax.com>
2023-04-22 11:39:18 -0500

    consistently use "var"

--- src/main/java/org/example/SimpleExample.java
+++ src/main/java/org/example/SimpleExample.java
@@ -8,16 +8,23 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
+/**
+ * A simple example of how to use the HNSW graph implementation.
+ *
+ * Creates a random universe of vectors, and performs search of random vectors against it.
+ *
+ * No correctness checks are performed, this is just an example of the API.
+ */
 public class SimpleExample {
     private static final VectorSimilarityFunction similarityFunction = VectorSimilarityFunction.COSINE;
     private static final Random random = new Random();
 
     public static void main(String[] args) throws IOException {
         // Create a random vector universe
-        int vectorDimensions = 1500;
-        int universeSize = 10_000;
+        var vectorDimensions = 1500;
+        var universeSize = 10_000;
         var universe = new ArrayList<float[]>(universeSize);
-        for (int i = 0; i < universeSize; i++) {
+        for (var i = 0; i < universeSize; i++) {
             universe.add(randomVector(vectorDimensions));
         }
 
@@ -28,7 +35,7 @@ public class SimpleExample {
 
         // search for the nearest neighbors of a random vector
         var queryVector = randomVector(vectorDimensions);
-        NeighborQueue nn = HnswGraphSearcher.search(queryVector, 10, ravv.copy(), VectorEncoding.FLOAT32, similarityFunction, hnsw, null, Integer.MAX_VALUE);
+        var nn = HnswGraphSearcher.search(queryVector, 10, ravv.copy(), VectorEncoding.FLOAT32, similarityFunction, hnsw, null, Integer.MAX_VALUE);
         System.out.println("Nearest neighbors of " + Arrays.toString(queryVector) + ":");
         for (var i : nn.nodes()) {
             var neighbor = universe.get(i);
@@ -38,8 +45,8 @@ public class SimpleExample {
     }
 
     private static float[] randomVector(int vectorDimension) {
-        float[] queryVector = new float[vectorDimension];
-        for (int i = 0; i < vectorDimension; i++) {
+        var queryVector = new float[vectorDimension];
+        for (var i = 0; i < vectorDimension; i++) {
             queryVector[i] = random.nextFloat();
         }
         return queryVector;

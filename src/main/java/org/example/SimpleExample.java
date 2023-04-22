6539a603b939e51e2a9e88411c29547d7cdd943e
Jonathan Ellis <jbellis@datastax.com>
2023-04-22 11:25:53 -0500

    don't use MockVectorValues, it does weird random stuff intended for testing

--- src/main/java/org/example/SimpleExample.java
+++ src/main/java/org/example/SimpleExample.java
@@ -2,9 +2,9 @@ package org.example;
 
 import org.apache.lucene.index.*;
 import org.apache.lucene.util.hnsw.*;
-import org.example.lucenecopy.MockVectorValues;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -16,13 +16,13 @@ public class SimpleExample {
         // Create a random vector universe
         int vectorDimensions = 1500;
         int universeSize = 10_000;
-        var universe = new float[universeSize][];
+        var universe = new ArrayList<float[]>(universeSize);
         for (int i = 0; i < universeSize; i++) {
-            universe[i] = randomVector(vectorDimensions);
+            universe.add(randomVector(vectorDimensions));
         }
 
         // construct a HNSW graph of the universe
-        var ravv = MockVectorValues.fromValues(universe);
+        var ravv = new ListRandomAccessVectorValues(universe, vectorDimensions);
         var builder = HnswGraphBuilder.create(ravv, VectorEncoding.FLOAT32, similarityFunction, 16, 100, random.nextInt());
         var hnsw = builder.build(ravv.copy());
 
@@ -31,7 +31,7 @@ public class SimpleExample {
         NeighborQueue nn = HnswGraphSearcher.search(queryVector, 10, ravv.copy(), VectorEncoding.FLOAT32, similarityFunction, hnsw, null, Integer.MAX_VALUE);
         System.out.println("Nearest neighbors of " + Arrays.toString(queryVector) + ":");
         for (var i : nn.nodes()) {
-            var neighbor = universe[i];
+            var neighbor = universe.get(i);
             var similarity = similarityFunction.compare(queryVector, neighbor);
             System.out.println("  " + Arrays.toString(neighbor) + " (similarity: " + similarity + ")");
         }

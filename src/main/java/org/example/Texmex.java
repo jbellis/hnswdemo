c3ace9a133055db1d963fb52227afb52746d234a
Jonathan Ellis <jbellis@datastax.com>
2023-07-30 17:09:34 -0500

    running vanilla hnsw against hdf5 datasets

--- src/main/java/org/example/Texmex.java
+++ src/main/java/org/example/Texmex.java
@@ -1,164 +1,175 @@
 package org.example;
 
+import io.jhdf.HdfFile;
 import org.apache.lucene.index.VectorEncoding;
 import org.apache.lucene.index.VectorSimilarityFunction;
-import org.apache.lucene.util.NamedThreadFactory;
 import org.apache.lucene.util.VectorUtil;
 import org.apache.lucene.util.hnsw.*;
 import org.example.util.ListRandomAccessVectorValues;
 
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.DoubleAdder;
 import java.util.stream.IntStream;
 
 /**
  * Tests HNSW against vectors from the Texmex dataset
  */
 public class Texmex {
-    private static String siftName = "sift";
-
-    public static ArrayList<float[]> readFvecs(String filePath) throws IOException {
-        var vectors = new ArrayList<float[]>();
-        try (var dis = new DataInputStream(new BufferedInputStream(new FileInputStream(filePath)))) {
-            while (dis.available() > 0) {
-                var dimension = Integer.reverseBytes(dis.readInt());
-                assert dimension > 0 : dimension;
-                var buffer = new byte[dimension * Float.BYTES];
-                dis.readFully(buffer);
-                var byteBuffer = ByteBuffer.wrap(buffer).order(ByteOrder.LITTLE_ENDIAN);
-
-                var vector = new float[dimension];
-                for (var i = 0; i < dimension; i++) {
-                    vector[i] = byteBuffer.getFloat();
-                }
-                VectorUtil.l2normalize(vector);
-                vectors.add(vector);
-            }
-        }
-        return vectors;
+    private record Results(int topK, double recall, long buildNanos, long fingerNanos, long queryNanos) {
     }
 
-    private static ArrayList<HashSet<Integer>> readIvecs(String filename) {
-        var groundTruthTopK = new ArrayList<HashSet<Integer>>();
-
-        try (var dis = new DataInputStream(new FileInputStream(filename))) {
-            while (dis.available() > 0) {
-                var numNeighbors = Integer.reverseBytes(dis.readInt());
-                var neighbors = new HashSet<Integer>(numNeighbors);
-
-                for (var i = 0; i < numNeighbors; i++) {
-                    var neighbor = Integer.reverseBytes(dis.readInt());
-                    neighbors.add(neighbor);
-                }
-
-                groundTruthTopK.add(neighbors);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return groundTruthTopK;
-    }
-
-    public static double testRecall(ArrayList<float[]> baseVectors, ArrayList<float[]> queryVectors, ArrayList<HashSet<Integer>> groundTruth) throws IOException, InterruptedException, ExecutionException {
+    public static Results testRecall(List<float[]> baseVectors, List<float[]> queryVectors, List<Set<Integer>> groundTruth) throws IOException {
         var ravv = new ListRandomAccessVectorValues(baseVectors, baseVectors.get(0).length);
+        var topK = groundTruth.get(0).size();
 
         var start = System.nanoTime();
-        var builder = ConcurrentHnswGraphBuilder.create(ravv, VectorEncoding.FLOAT32, VectorSimilarityFunction.DOT_PRODUCT, 16, 100);
-        int buildThreads = 24;
-        var es = Executors.newFixedThreadPool(
-                        buildThreads, new NamedThreadFactory("Concurrent HNSW builder"));
-        var hnsw = builder.buildAsync(ravv.copy(), es, buildThreads).get();
-        es.shutdown();
-        System.out.printf("  Building index took %s seconds%n", (System.nanoTime() - start) / 1_000_000_000.0);
+        var builder = HnswGraphBuilder.create(ravv, VectorEncoding.FLOAT32, VectorSimilarityFunction.DOT_PRODUCT, 16, 100, 42);
+        var hnsw = builder.build(ravv.copy());
+        long buildNanos = System.nanoTime() - start;
 
         start = System.nanoTime();
-        FingerMetadata<float[]> fm = new FingerMetadata<>(hnsw.getView(), ravv, VectorEncoding.FLOAT32, VectorSimilarityFunction.DOT_PRODUCT, 64);
-        System.out.printf("  Finger metadata created in %s seconds%n", (System.nanoTime() - start) / 1_000_000_000.0);
+        FingerMetadata<float[]> fm = null; // new FingerMetadata<>(hnsw.getView(), ravv, VectorEncoding.FLOAT32, VectorSimilarityFunction.DOT_PRODUCT, 64);
+        long fingerNanos = System.nanoTime() - start;
 
-        var topKfound = new AtomicInteger(0);
-        var topK = 100;
         start = System.nanoTime();
 
-        int queryRuns = 100;
-        performQueries(queryVectors, groundTruth, ravv, hnsw, fm, topKfound, topK, queryRuns);
-        System.out.printf("  Querying %d vectors x10 in parallel took %s seconds%n", queryVectors.size(), (System.nanoTime() - start) / 1_000_000_000.0);
-        return (double) topKfound.get() / (queryRuns * queryVectors.size() * topK);
+        int queryRuns = 10;
+        int topKFound = performQueries(queryVectors, groundTruth, ravv, hnsw, fm, topK, queryRuns);
+        long queryNanos = System.nanoTime() - start;
+        var recall = ((double) topKFound) / (queryRuns * queryVectors.size() * topK);
+        return new Results(topK, recall, buildNanos, fingerNanos, queryNanos);
     }
 
-    private static void performQueries(ArrayList<float[]> queryVectors, ArrayList<HashSet<Integer>> groundTruth, ListRandomAccessVectorValues ravv, ConcurrentOnHeapHnswGraph hnsw, FingerMetadata<float[]> fm, AtomicInteger topKfound, int topK, int queryRuns) {
+    private static float normOf(float[] baseVector) {
+        float norm = 0;
+        for (float v : baseVector) {
+            norm += v * v;
+        }
+        return (float) Math.sqrt(norm);
+    }
+
+    private static int performQueries(List<float[]> queryVectors, List<Set<Integer>> groundTruth, ListRandomAccessVectorValues ravv, HnswGraph hnsw, FingerMetadata<float[]> fm, int topK, int queryRuns) throws IOException {
+        int topKfound = 0;
         for (int k = 0; k < queryRuns; k++) {
-            ThreadLocal<HnswSearcher<float[]>> searchers = ThreadLocal.withInitial(() -> {
-                return new HnswSearcher.Builder<>(hnsw.getView(), ravv, VectorEncoding.FLOAT32, VectorSimilarityFunction.DOT_PRODUCT)
+            HnswSearcher<float[]> searcher = new HnswSearcher.Builder<>(hnsw, ravv, VectorEncoding.FLOAT32, VectorSimilarityFunction.DOT_PRODUCT)
                         .withFinger(fm)
                         .build();
-            });
-            IntStream.range(0, queryVectors.size()).parallel().forEach(i -> {
+            for (int i = 0; i < queryVectors.size(); i++) {
                 var queryVector = queryVectors.get(i);
                 NeighborQueue nn;
-                try {
-                    var searcher = searchers.get();
-                    nn = searcher.search(queryVector, topK, null, Integer.MAX_VALUE);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+                nn = searcher.search(queryVector, topK, null, Integer.MAX_VALUE);
 
                 var gt = groundTruth.get(i);
                 int[] resultNodes = nn.nodes();
                 var n = IntStream.range(0, Math.min(nn.size(), topK)).filter(j -> gt.contains(resultNodes[j])).count();
-                topKfound.addAndGet((int) n);
-            });
+                topKfound += n;
+            }
         }
+        return topKfound;
     }
 
-    public static void main(String[] args) throws IOException {
-        if (args.length > 0) {
-            siftName = args[0];
-        }
+    private static void computeRecallFor(String pathStr) throws IOException {
+        HdfFile hdf = new HdfFile(Paths.get(pathStr));
+        float[][] baseVectors = (float[][]) hdf.getDatasetByPath("train").getData();
+        float[][] queryVectors = (float[][]) hdf.getDatasetByPath("test").getData();
+        int[][] groundTruth = (int[][]) hdf.getDatasetByPath("neighbors").getData();
 
-        System.out.println("Heap space available is " + Runtime.getRuntime().maxMemory());
-        var baseVectors = readFvecs(String.format("%s/%s_base.fvecs", siftName, siftName));
-        var queryVectors = readFvecs(String.format("%s/%s_query.fvecs", siftName, siftName));
-        var groundTruth = readIvecs(String.format("%s/%s_groundtruth.ivecs", siftName, siftName));
-        System.out.format("%d base and %d query vectors loaded, dimensions %d%n",
-                baseVectors.size(), queryVectors.size(), baseVectors.get(0).length);
-
-        // Average recall and standard deviation over multiple runs
-        var numRuns = 1;
-
-        var totalRecall = new DoubleAdder();
-        var totalRecallSquared = new DoubleAdder();
-
-        IntStream.range(0, numRuns)
-                .mapToDouble(i -> {
-                    System.out.printf("Run %d:%n", i);
-                    try {
-                        return testRecall(baseVectors, queryVectors, groundTruth);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        return 0;
+        // verify that vectors are normalized and sane
+        List<float[]> scrubbedBaseVectors = new ArrayList<>(baseVectors.length);
+        List<float[]> scrubbedQueryVectors = new ArrayList<>(queryVectors.length);
+        List<Set<Integer>> gtSet = new ArrayList<>(groundTruth.length);
+        if (Math.abs(normOf(baseVectors[0]) - 1.0) > 1e-5) {
+            System.out.println("Normalizing vectors for " + pathStr);
+            for (float[] v : baseVectors) {
+                try {
+                    VectorUtil.l2normalize(v);
+                    scrubbedBaseVectors.add(v);
+                } catch (Throwable th) {
+                    // System.out.println("Skipping vector with norm " + normOf(v));
+                }
+            }
+            for (int i = 0; i < queryVectors.length; i++) {
+                float[] v = queryVectors[i];
+                try {
+                    // do it in this order so if the normalize fails for being length 0,
+                    // we don't add it to the query or ground truth sets
+                    VectorUtil.l2normalize(v);
+                    scrubbedQueryVectors.add(v);
+                    var gt = new HashSet<Integer>();
+                    for (int j = 0; j < groundTruth[i].length; j++) {
+                        gt.add(groundTruth[i][j]);
                     }
-                })
-                .forEach(recall -> {
-                    totalRecall.add(recall);
-                    totalRecallSquared.add(recall * recall);
-                });
+                    gtSet.add(gt);
+                } catch (Throwable th) {
+                    // System.out.println("Skipping vector with norm " + normOf(v));
+                }
+            }
+        } else {
+            for (float[] v : baseVectors) {
+                if (Math.abs(normOf(v)) > 1e-5) {
+                    scrubbedBaseVectors.add(v);
+                }
+            }
+            for (int i = 0; i < queryVectors.length; i++) {
+                float[] v = queryVectors[i];
+                if (Math.abs(normOf(v)) > 1e-5) {
+                    scrubbedQueryVectors.add(v);
+                    var gt = new HashSet<Integer>();
+                    for (int j = 0; j < groundTruth[i].length; j++) {
+                        gt.add(groundTruth[i][j]);
+                    }
+                    gtSet.add(gt);
+                }
+            }
+        }
+        assert scrubbedQueryVectors.size() == gtSet.size();
+        baseVectors = null;
+        queryVectors = null;
+        groundTruth = null;
 
-        var averageRecall = totalRecall.doubleValue() / numRuns;
-        var variance = (totalRecallSquared.doubleValue() / numRuns) - (averageRecall * averageRecall);
-        var stdev = Math.sqrt(variance);
+        System.out.format("%s: %d base and %d query vectors loaded, dimensions %d%n",
+                pathStr, scrubbedBaseVectors.size(), scrubbedQueryVectors.size(), scrubbedBaseVectors.get(0).length);
 
-        System.out.println("Average Recall: " + averageRecall);
-        System.out.println("Standard Deviation: " + stdev);
+        var results = testRecall(scrubbedBaseVectors, scrubbedQueryVectors, gtSet);
+        System.out.format("%s: top %d recall %.4f, build %.2fs, finger %.2fs, query %.2fs%n",
+                pathStr, results.topK, results.recall, results.buildNanos / 1_000_000_000.0, results.fingerNanos / 1_000_000_000.0, results.queryNanos / 1_000_000_000.0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Heap space available is " + Runtime.getRuntime().maxMemory());
+
+        new Thread(() -> {
+            try {
+                computeRecallFor("hdf5/glove-100-angular.hdf5");
+            } catch (Throwable e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                computeRecallFor("hdf5/glove-200-angular.hdf5");
+            } catch (Throwable e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                computeRecallFor("hdf5/deep-image-96-angular.hdf5");
+            } catch (Throwable e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                computeRecallFor("hdf5/nytimes-256-angular.hdf5");
+            } catch (Throwable e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
     }
 }
